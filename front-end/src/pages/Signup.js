70959921371c004020cdf80fea0b5d7c250b4a28
fcthevan123-vn <fcthevan123@gmail.com>
2023-04-04 22:24:52 +0700

    fix signup page css

--- front-end/src/pages/Signup.js
+++ front-end/src/pages/Signup.js
@@ -26,84 +26,95 @@ function Signup() {
         <Container>
           <Row>
             <Col md={6} className="signup__form--container">
-              <Form
-                style={{ width: "100%" }}
-                onSubmit={handleSignup}
-                className="form-wrapper"
-              >
-                <h4 className="fs-22">Đăng ký tài khoản ngay</h4>
-                {isError && (
-                  <Alert variant="danger" className="fs-16">
-                    {error.data}
-                  </Alert>
-                )}
-                <Form.Group>
-                  <Form.Label className="d-flex fs-16">Họ và tên: </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Họ và tên của bạn"
-                    value={name}
-                    required
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Form.Group>
-                  <Form.Label className="d-flex fs-16 my-3">
-                    Mã số sinh viên:{" "}
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Mã số sinh viên của bạn"
-                    value={studentID}
-                    required
-                    onChange={(e) => setStudentID(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Form.Group className="my-3">
-                  <Form.Label className="d-flex fs-16">
-                    Địa chỉ email:{" "}
-                  </Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Nhập email của bạn"
-                    value={email}
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Form.Group className="my-3">
-                  <Form.Label className="d-flex fs-16">
-                    Nhập mật khẩu
-                  </Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Nhập mật khẩu"
-                    value={password}
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Form.Group className="mt-4">
-                  <Button
-                    type="submit"
-                    disabled={isLoading}
-                    className="w-25 rounded-pill fs-14"
+              <div className="card-signup">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="card-signup-inner">
+                  <Form
+                    style={{ width: "100%" }}
+                    onSubmit={handleSignup}
+                    className="form-wrapper"
                   >
-                    Đăng ký ngay
-                  </Button>
-                </Form.Group>
-                <p className="mt-3 text-center fs-14 text-description">
-                  Đã có tài khoản? <Link to="/login">Đăng nhập ngay</Link>{" "}
-                </p>
-              </Form>
+                    <h4 className="fs-22">Đăng ký tài khoản ngay</h4>
+                    {isError && (
+                      <Alert variant="danger" className="fs-16">
+                        {error.data}
+                      </Alert>
+                    )}
+                    <Form.Group>
+                      <Form.Label className="d-flex fs-16">
+                        Họ và tên:{" "}
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Họ và tên của bạn"
+                        value={name}
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label className="d-flex fs-16 my-3">
+                        Mã số sinh viên:{" "}
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Mã số sinh viên của bạn"
+                        value={studentID}
+                        required
+                        onChange={(e) => setStudentID(e.target.value)}
+                      />
+                    </Form.Group>
+
+                    <Form.Group className="my-3">
+                      <Form.Label className="d-flex fs-16">
+                        Địa chỉ email:{" "}
+                      </Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="Nhập email của bạn"
+                        value={email}
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </Form.Group>
+
+                    <Form.Group className="my-3">
+                      <Form.Label className="d-flex fs-16">
+                        Nhập mật khẩu
+                      </Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="Nhập mật khẩu"
+                        value={password}
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </Form.Group>
+
+                    <Form.Group className="mt-4">
+                      <button type="submit" className="fs-16 btn-signup">
+                        Đăng ký ngay
+                      </button>
+                      {/* <Button
+                        type="submit"
+                        disabled={isLoading}
+                        className="w-25 rounded-pill fs-14 btn-signup"
+                      >
+                        Đăng ký ngay
+                      </Button> */}
+                    </Form.Group>
+                    <p className="mt-3 text-center fs-14 text-description">
+                      Đã có tài khoản? <Link to="/login">Đăng nhập ngay</Link>{" "}
+                    </p>
+                  </Form>
+                </div>
+              </div>
             </Col>
             <Col md={1}></Col>
             <Col md={5} className="col-img">
-              <div className="signup__image--container"></div>
+              <div className="signup__image--container gradient-border"></div>
             </Col>
           </Row>
         </Container>
