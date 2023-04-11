c3e8633e8a4df264413ca11860a7fce67ef29aeb
fcthevan123-vn <fcthevan123@gmail.com>
2023-04-11 23:45:05 +0700

    test deploy

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,7 @@ require("./connection");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: "http://localhost:3001",
+  cors: "https://library-ctu-app.onrender.com",
   methods: ["GET", "POST", "PATCH", "DELETE"],
 });
 
